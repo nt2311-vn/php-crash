796433e0a420ac811b3105e969d26612507b4f3d
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-12 00:00:02 +0700

    explore php syntax

--- _starter_files/01_output.php
+++ _starter_files/01_output.php
@@ -1,6 +1,9 @@
 <?php
+
 /* -------- Output & Comments ------- */
 
 /*
   A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
 */
+echo 'Hello123';
+
